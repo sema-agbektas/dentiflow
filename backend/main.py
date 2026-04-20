21bff109961ad06feec2c5a16856614b68327e7f
sema-agbektas <semanurzeynep5858@gmail.com>
2026-04-20 03:51:55 +0300

    fix: CORS guncellendiii

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://dentiflow-app.vercel.app",
+        "https://dentiflow-app.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
